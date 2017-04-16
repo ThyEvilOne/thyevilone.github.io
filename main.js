27d860bcb5de295fc3a62b20142e3b6f9b0d81a7
Noah Works <noah.works6@gmail.com>
2017-04-16 18:11:54 +0000

    fixed chat, added button / more autism

--- main.js
+++ main.js
@@ -8,30 +8,40 @@
   };
   firebase.initializeApp(config);
   
-  var chatData = firebase.database().ref();
-  
-  function pushMessage(event){
-      if(event.keycode == 13){
-          var name = $('#nameInput').val();
-          var text = $('#messageInput').val();
-          
-          chatData.push({name: name, text: text});
-          $('#messageInput').val('');
-      }
+ var chatData = firebase.database().ref();
+
+function pushMessage(event) {
+        if (event.keyCode == 13) {
+            var name = $('#nameInput').val();
+            var text = $('#messageInput').val();
+            chatData.push({name: name, text: text});
+            $('#messageInput').val('');
   }
   
-  function showMessage(msg){
-      //get message OBJECT from FireBase
-      var message = msg.val();
-      var messageSender = message.name;
-      var messageContent = message.text;
-      
-      var messageEl = $("<div/>").addClass("message");
-      var senderEl = $("<span/>").text(messageSender + ": ");
-      var contentEl = $("<span/>").text(messageContent);
-      
-      //addeds the text to the element
-      messageEl.append(senderEl);
-      messageEl.append(contentEl);
-      $('#messages').append(messageEl)
-  }
+}
+
+function pushMessageButton(){
+        var name = $('#nameInput').val();
+        var text = $('#messageInput').val();
+        chatData.push({name: name, text: text});
+        $('#messageInput').val('');
+}
+
+$('#messageInput').keypress(pushMessage);
+$("#send").on("click", pushMessageButton);
+
+chatData.on("child_added", showMessage);
+
+function showMessage(msg) {
+        var message = msg.val();
+        var messageSender = message.name;
+        var messageContent = message.text;
+
+        var messageEl = $("<div/>").addClass("message");
+        var senderEl = $("<span/>").text(messageSender + ": ");
+        var contentEl = $("<span/>").text(messageContent);
+  
+        messageEl.append(senderEl);
+        messageEl.append(contentEl);
+        $('#messages').append(messageEl);
+}
